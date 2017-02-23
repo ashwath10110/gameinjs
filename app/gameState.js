8487a6f07908ca6490c793a6c971605297da7c62
Ashwath Bharadwaj <ashwath10110@gmail.com>
2017-02-23 19:40:09 +0530

    levels added

--- app/gameState.js
+++ app/gameState.js
@@ -12,19 +12,34 @@ var GameState = State.extend({
 		this.ship.maxX = this.canvasWidth;
 		this.ship.maxY = this.canvasHeight;
 
+		this.lvl = 0;
+
 		this.generateLvl();
 	},
 
 	generateLvl: function() {
-		var num = 3;
+		var num = Math.round((this.lvl + 5) / 10 + 3);
+
+		this.ship.x = this.canvasWidth / 2;
+		this.ship.y = this.canvasHeight / 2;
 
 		this.asteroids = [];
-		this.bullet = [];
+		this.bullets = [];
 
 		for (var i = 0; i < num; i++) {
 
 			var n = Math.round(Math.random() * (Points.ASTEROIDS.length - 1));
-			var astr = new Asteroid(Points.ASTEROIDS[n], AsteroidSize, 100, 100);
+
+			var x = 0,
+				y = 0;
+
+			if (Math.random() > 0.5) {
+				x = Math.random() * this.canvasWidth;
+			} else {
+				y = Math.random() * this.canvasHeight;
+			}
+
+			var astr = new Asteroid(Points.ASTEROIDS[n], AsteroidSize, x, y);
 
 			astr.maxX = this.canvasWidth;
 			astr.maxY = this.canvasHeight;
@@ -51,41 +66,75 @@ var GameState = State.extend({
 		}
 
 		if (input.isPressed("spacebar")) {
-			this.bullet.push(this.ship.shoot());
+			this.bullets.push(this.ship.shoot());
 		}
 	},
 
 	update: function() {
 		for (var i = 0, len = this.asteroids.length; i < len; i++) {
-			this.asteroids[i].update();
+			var a = this.asteroids[i];
+			a.update();
+
+			for (var j = 0, len2 = this.bullets.length; j < len2; j++) {
+				var b = this.bullets[j];
+
+				if (a.hasPoint(b.x, b.y)) {
+					this.bullets.splice(j, 1);
+					len2--;
+					j--;
+
+					if (a.size > AsteroidSize / 4) {
+
+						for (var k = 0; k < 2; k++) {
+
+							var n = Math.round(Math.random() * (Points.ASTEROIDS.length - 1));
+							var astr = new Asteroid(Points.ASTEROIDS[n], a.size / 2, a.x, a.y);
+
+							astr.maxX = this.canvasWidth;
+							astr.maxY = this.canvasHeight;
+
+							this.asteroids.push(astr);
+
+							len++;
+						}
+					}
+					this.asteroids.splice(i, 1);
+					len--;
+					i--;
+				}
+			}
 		}
 
-		for (var i = 0, len = this.bullet.length; i < len; i++) {
-			var b = this.bullet[i];
+		for (var i = 0, len = this.bullets.length; i < len; i++) {
+			var b = this.bullets[i];
 			b.update();
 
 			if (b.shallRemove) {
-				this.bullet.splice(i, 1);
+				this.bullets.splice(i, 1);
 				len--;
 				i--;
 			}
 		}
 		this.ship.update();
 
+		if (this.asteroids.length == 0) {
+			this.lvl++;
+			this.generateLvl();
+		}
 	},
 
 	render: function(ctx) {
-		
+
 		ctx.clearAll();
-		
+
 		for (var i = 0, len = this.asteroids.length; i < len; i++) {
 			this.asteroids[i].draw(ctx);
 		}
 
 		this.ship.draw(ctx);
 
-		for (var i = 0, len = this.bullet.length; i < len; i++) {
-			this.bullet[i].draw(ctx);
+		for (var i = 0, len = this.bullets.length; i < len; i++) {
+			this.bullets[i].draw(ctx);
 		}
 	}
 });
